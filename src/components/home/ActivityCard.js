3b65d57db986ca1c454b531b48c9070c912734a8
Ember Parr <ember-devops@gmail.com>
2020-11-09 13:06:47 -0600

    added ability to click recent activity card to view details

--- src/components/home/ActivityCard.js
+++ src/components/home/ActivityCard.js
@@ -2,6 +2,7 @@
 /* eslint-disable array-callback-return */
 import React, {useContext, useEffect } from 'react'
 import { Grid, Card } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom'
 import { ActivityContext } from './ActivityProvider'
 import { ConnectionContext } from "../connectedUsers/ConnectionProvider";
 
@@ -11,13 +12,12 @@ export const ActivityCard = () => {
     const { connections, getConnection } = useContext(ConnectionContext);
     const connectedUsers = connections.filter(connection => connection.userId === currentUser)
     const connectedUsersIdList = connectedUsers.map(connection => connection.connectedUserId)
+    const history = useHistory()
 
     useEffect(()=> {
         getActivities().then(getConnection)
     }, [])
 
-    console.log("connected user ids: ", connectedUsersIdList)
-    console.log("activities: ", Activities)
 
     return (
         <>
@@ -28,7 +28,7 @@ export const ActivityCard = () => {
                             return (
                                 <>
 
-                        <Card key={activity.id} className="recentActivityCard">
+                        <Card key={activity.id} className="recentActivityCard" onClick={() => history.push(`/items/edit/${activity.itemId}`)}>
                             <Card.Content>
                                 <Card.Header>
                                     {activity?.user?.firstName} {activity?.user?.lastName}
@@ -38,6 +38,25 @@ export const ActivityCard = () => {
                                 </Card.Meta>
                                 <Card.Description>
                                     {activity.activityType}: {activity?.item?.itemName} 
+                                </Card.Description>
+                        </Card.Content>
+                    </Card>
+                                </>
+                            )
+                        } else if (connectedUsersIdList.includes(activity.userId) && activity.locationId !== 1 && activity.itemId === 1 && activity.connectedUserId === 1) {
+                            return (
+                                <>
+
+                        <Card key={activity.id} className="recentActivityCard"  onClick={() => history.push(`/locations/edit/${activity.locationId}`)}>
+                            <Card.Content>
+                                <Card.Header>
+                                    {activity?.user?.firstName} {activity?.user?.lastName}
+                                </Card.Header>
+                                <Card.Meta>
+                                    {activity.date}
+                                </Card.Meta>
+                                <Card.Description>
+                                    {activity.activityType}: {activity?.location?.name} 
                                 </Card.Description>
                         </Card.Content>
                     </Card>
